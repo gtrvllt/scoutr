7a2fa176413e6d068af85c2ff3338ea8a38a6b32
gtrvllt <gautier.vaillant@epitech.eu>
2025-04-08 17:47:48 +0200

    ajout meta fonctionnl

--- app/components/AddMeta.tsx
+++ app/components/AddMeta.tsx
@@ -1,25 +1,101 @@
 "use client";
 import React, { useState } from "react";
 import { supabase } from "@/lib/supabase";
+import Image from "next/image";
 
 const AddMeta = () => {
   const [creatingMeta, setCreatingMeta] = useState(false);
   // metadata
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [type, setType] = useState("");
-  // metadata
+  const [formData, setFormData] = useState<{
+    name: string;
+    description: string;
+    type: string;
+    file: File | null; // 👈 Permet d'accepter un fichier ou null
+  }>({
+    name: "",
+    description: "",
+    type: "",
+    file: null,
+  });
+  const [preview, setPreview] = useState<string | null>(null); // Stocke l'URL de prévisualisation
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFormData((prevData) => ({
+      ...prevData,
+      file: file,
+    }));
+
+    if (file) {
+      setPreview(URL.createObjectURL(file));
+    } else {
+      setPreview(null);
+    }
+  };
+
   const addMetaClick = () => {
     // prmier click : ouvrir le formulaire en setant creatingmeta à true
     // deuxieme click : on vérifie que les champs sont remplis et on envoie la requete, puis on ferme le formulaire en setant creatingmeta à false
     setCreatingMeta(!creatingMeta);
+    if (creatingMeta && formData.file !== null) {
+      createMeta(formData);
+      setCreatingMeta(false);
+    }
   };
 
-  const createMeta = async (metaData: {
+  const uploadImage = async (file: File) => {
+    const { data, error } = await supabase.storage
+      .from("images")
+      .upload(`public/${file.name}`, file);
+      // todo - gérer le nom du dossier
+    if (error) {
+      console.error("Erreur lors du téléchargement de l'image :", error);
+      return null;
+    }
+    return data.path;
+  };
+
+  const getPublicUrl = (imagePath: string | null) => {
+    if (!imagePath) return null;
+    // const { publicURL, error } = supabase.storage
+    const res = supabase.storage
+      .from('images')
+      .getPublicUrl(imagePath);
+    console.log('ABC getPublicUrl res', res)
+    // if (res.error) {
+    //   console.error('Erreur lors de la récupération de l\'URL publique :', error);
+    //   return null;
+    // }
+  
+    return res.data.publicUrl;
+  };
+
+  const createMeta = async (data: {
     name: string;
     description: string;
     type: string;
+    file: File;
   }) => {
+    const imagePath = await uploadImage(data.file);
+    const imageUrl = getPublicUrl(imagePath)
+    // const res =  await insertMeta(data, imagePath)
+    const res =  await insertMeta(data, imageUrl)
+  }
+
+
+  const insertMeta = async (metaData: {
+    name: string;
+    description: string;
+    type: string;
+  }, imagePath: string | null) => {
     // envoyer la requete
 
     const response = await supabase
@@ -29,8 +105,9 @@ const AddMeta = () => {
           name: metaData.name,
           description: metaData.description,
           type: metaData.type,
-          user_id: "fb7a25b6-d374-4d91-a658-2d8f4f9c90f1",
-          country_codes: ('["FR"]'),
+          user_id: "fb7a25b6-d374-4d91-a658-2d8f4f9c90f1", // current test user
+          country_codes: '["FR"]',
+          image_url: imagePath,
         },
       ])
       .select();
@@ -38,34 +115,52 @@ const AddMeta = () => {
   return (
     <div className="add-meta-container">
       {creatingMeta && (
-        <div className="add-meta-form">
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Name"
-            className="meta-name-input"
-          />
-          <input
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Description"
-            className="meta-description-input"
-          />
-          <input
-            type="text"
-            value={type}
-            onChange={(e) => setType(e.target.value)}
-            placeholder="Type"
-            className="meta-description-input"
-          />
-          <button
+        <div className="add-meta-formf flex">
+          <div className="image-container">
+            {/* {!formData.file ? ( */}
+            {!preview ? (
+              <input
+                type="file"
+                name="file"
+                onChange={handleFileChange}
+                style={{ marginRight: "10px" }}
+              />
+            ) : (
+              <Image src={preview} alt="metaImage" height={200} width={250} />
+            )}
+          </div>
+          <div className="meta-inputs flex flex-col">
+            <input
+              type="text"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Name"
+              name="name"
+              className="meta-name-input"
+            />
+            <input
+              type="text"
+              value={formData.description}
+              onChange={handleChange}
+              placeholder="Description"
+              name="description"
+              className="meta-description-input"
+            />
+            <input
+              type="text"
+              value={formData.type}
+              onChange={handleChange}
+              placeholder="Type"
+              name="type"
+              className="meta-description-input"
+            />
+            {/* <button
             className="save-meta-button"
             onClick={() => createMeta({ name, description, type })}
-          >
+            >
             Save
-          </button>
+            </button> */}
+          </div>
         </div>
       )}
       {/* bouton ajout meta */}
